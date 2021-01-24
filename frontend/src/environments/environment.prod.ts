004b0751b5e5b46d5857ae2e30d3e0939550cea6
Ajob Kustra <ajobkustra.p@gmail.com>
2021-01-24 20:16:09 +0100

    generic API URL, no cross origin

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ import { HttpHeaders } from "@angular/common/http";
 
 export const environment = {
   production: true,
-  API_URL: 'http://188.166.93.249:8080',
+  API_URL: window.location.origin + ':8080',
   DEFAULT_HTTP_OPTIONS: {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
     
