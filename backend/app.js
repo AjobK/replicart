358ee68209fe8d498d90f8105fa3f2d9ccbfa447
Ajob Kustra <ajobkustra.p@gmail.com>
2021-01-27 14:47:47 +0100

    Added back routes

--- backend/app.js
+++ backend/app.js
@@ -34,7 +34,7 @@ app.use((req, res, next) => {
     });
     
     // // Initialize routes with /api prefix
-    // app.use('/api', require('./generalRouter'));
+    app.use('/api', require('./generalRouter'));
     
     const httpsServer = https.createServer({
         key: fs.readFileSync('/etc/letsencrypt/live/kustra.nl/privkey.pem'),
