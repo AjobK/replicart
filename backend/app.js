82ebbcc3eae4206f79e4bc30db16a50a7c4b6ce6
Ajob Kustra <ajobkustra.p@gmail.com>
2021-01-27 14:45:28 +0100

    added back backend funcs

--- backend/app.js
+++ backend/app.js
@@ -14,26 +14,26 @@ const app = express();
 
 console.log('im fine here 1')
 
-// app.use(bodyParser.json());
+app.use(bodyParser.json());
 console.log('im fine here 2')
-// app.use(cookieParser())
+app.use(cookieParser())
 console.log('im fine here 3')
-// app.use(cors({
-//     origin: [FRONTEND_URL, 'http://kustra.nl', 'https://kustra.nl', 'http://localhost:4200'],
-//     credentials: true
-// }))
+app.use(cors({
+    origin: [FRONTEND_URL, 'http://kustra.nl', 'https://kustra.nl', 'http://localhost:4200'],
+    credentials: true
+}))
 console.log('im fine here 4')
 
 // External access (CORS)
-// app.use((req, res, next) => {
-    //     res.setHeader('Access-Control-Allow-Origin', '*');                              // Allow client to send requests from given origin, * serving as a wildcard
-    //     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE'); // Allow client to use given request methods
-    //     res.setHeader('Access-Control-Allow-Headers', '*');   // Allow client to send request headers
-    //     res.setHeader('Access-Control-Allow-Credentials', true);   // Allow client to send request headers
-    //     next();
-    // });
+app.use((req, res, next) => {
+        res.setHeader('Access-Control-Allow-Origin', '*');                              // Allow client to send requests from given origin, * serving as a wildcard
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE'); // Allow client to use given request methods
+        res.setHeader('Access-Control-Allow-Headers', '*');   // Allow client to send request headers
+        res.setHeader('Access-Control-Allow-Credentials', true);   // Allow client to send request headers
+        next();
+    });
     
-    // Initialize routes with /api prefix
+    // // Initialize routes with /api prefix
     // app.use('/api', require('./generalRouter'));
     
     const httpsServer = https.createServer({
