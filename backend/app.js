aaaeaec856e5744ea93a0928780c073941fe96bc
Ajob Kustra <ajobkustra.p@gmail.com>
2021-01-27 16:52:54 +0100

    Added .env gitignore

--- backend/app.js
+++ backend/app.js
@@ -9,44 +9,28 @@ const cors = require('cors');
 
 require('dotenv').config()
 
-const { FRONTEND_URL } = process.env;
+const { FRONTEND_URL, SECURE } = process.env;
 const app = express();
 
-console.log('im fine here 1')
-
 app.use(bodyParser.json());
-console.log('im fine here 2')
-app.use(cookieParser())
-console.log('im fine here 3')
+app.use(cookieParser());
 app.use(cors({
     origin: [FRONTEND_URL, 'http://kustra.nl', 'https://kustra.nl', 'http://localhost:4200'],
     credentials: true
-}))
-console.log('im fine here 4')
-
-// External access (CORS)
-// app.use((req, res, next) => {
-//         res.setHeader('Access-Control-Allow-Origin', '*');                              // Allow client to send requests from given origin, * serving as a wildcard
-//         res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE'); // Allow client to use given request methods
-//         res.setHeader('Access-Control-Allow-Headers', '*');   // Allow client to send request headers
-//         res.setHeader('Access-Control-Allow-Credentials', true);   // Allow client to send request headers
-//         next();
-//     });
+}));
 
 // // Initialize routes with /api prefix
 app.use('/api', require('./generalRouter'));
 
-const httpsServer = https.createServer({
-    key: fs.readFileSync('/etc/letsencrypt/live/kustra.nl/privkey.pem'),
-    cert: fs.readFileSync('/etc/letsencrypt/live/kustra.nl/fullchain.pem')
-}, app);
-console.log('im fine here 5')
+if (SECURE == 'true') {
+    const httpsServer = https.createServer({
+        key: fs.readFileSync('/etc/letsencrypt/live/kustra.nl/privkey.pem'),
+        cert: fs.readFileSync('/etc/letsencrypt/live/kustra.nl/fullchain.pem')
+    }, app);
 
-httpsServer.listen(8080, () => {
-    console.log('HTTPS Server runnign on port 8080 :-)');
-})
-console.log('im fine here 6')
-    
-
-
-// app.listen(8080);
+    httpsServer.listen(8080, () => {
+        console.log('(Secure) HTTPS Server running on port 8080 :-)');
+    })
+} else {
+    app.listen(8080, () => { console.log('(Insecure) HTTP Server running on port 8080 :-)')})
+}
